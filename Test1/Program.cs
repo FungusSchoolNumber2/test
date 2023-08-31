50d53bc335dd9fdf7e7b31df9e8bd69d9b258107
FungusSchoolNumber2 <d23linst@student.his.se>
2023-08-31 14:52:21 +0200

    Tools kinda good

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -5,19 +5,18 @@ namespace Test1
     {
         static void Main(string[] args)
         {
+            Tools Tool = new Tools();
             int areaSize;
             int ships;
             bool multiplayer;
             gameSetup(out areaSize, out ships, out multiplayer);
-            Bot.drawGrid();
+            //Bot.drawGrid();
             Water player = new Water(5, 3, false);
             player.drawGrid();
         }
-
         static void gameSetup(out int areaSize, out int ships, out bool mutiplayer)
         {
             Console.Write("How big would you like the arena to be: ");
-            
         }
 
     }
--- Test1/Tools.cs
+++ Test1/Tools.cs
@@ -4,6 +4,57 @@ public class Tools
 {
     public int SafeInts(string input, int? maxValue = null, int? minValue = null)
     {
-        
+        int output;
+        while (true)
+        {
+            while (!int.TryParse(input, out output))
+            {
+                Console.WriteLine("That is not a valid number please try again!");
+                input = Console.ReadLine();
+            }
+
+            if (maxValue == null && minValue == null)
+            {
+                break;
+            }
+            else if (maxValue != null && minValue == null)
+            {
+                if (output >= maxValue)
+                {
+                    Console.WriteLine("The number needs to be under " + maxValue);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else if (maxValue == null && minValue != null)
+            {
+                if (output <= minValue)
+                {
+                Console.WriteLine("The number needs to be over " + minValue);
+                    
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                if ((output >= maxValue) && (output <= minValue))
+                {
+                    Console.WriteLine($"The number need to be between {minValue} and {maxValue}");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            input = "x";
+        }
+
+        return output;
     }
 }
